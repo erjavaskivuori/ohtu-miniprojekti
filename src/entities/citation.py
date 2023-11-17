3e0557da90b47930c2e359b7b97151e75cf8e022
Heljä Lehtinen <lehthelj@gmail.com>
2023-11-17 13:09:23 +0200

    Edited class citation

--- src/entities/citation.py
+++ src/entities/citation.py
@@ -7,6 +7,15 @@ class Citation():
         self.title = title
         self.year = year
 
+    def year_validator(self,year):
+        try:
+            i=int(year)
+            if i<0 or i>2040:
+                return False
+        except:
+            return False
+        return True
+
     def __str__(self):
         return f"{self.type}, {self.author}, {self.title}, {self.year}"
 
