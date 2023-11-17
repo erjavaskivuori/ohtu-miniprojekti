eb8b2a246812f9320c77a8785d5b36f4c1681e27
Heljä Lehtinen <lehthelj@gmail.com>
2023-11-17 13:31:48 +0200

    Edited class CitaionManager

--- src/logic/citationmanager.py
+++ src/logic/citationmanager.py
@@ -1,12 +1,26 @@
-from src.entities import Citation
+from entities import citation
 
 class CitationManager():
-    def __init__(self, citation = Citation):
-        self.citation = citation
+    def __init__(self):
+        self.all = []
 
     def add_citation(self, type, author, title, year):
-        new_citation = self.citation(type, author, title, year)
+        new_citation = citation.Citation(type, author, title, year)
+        self.all.append(new_citation)
         
-        return new_citation
-        
+    def return_all_citaions(self):
+        return self.all
+    
+    def print_all(self):
+        for citation in self.all:
+            print(citation)
+    
+    def delete_citation(self,citation):
+        None
 
+    def search_citation(self,citation):
+        None
+
+# if __name__=="__main__":
+#     testi = Citation("a","a","a",11)
+#     print(testi)
