7030c6c772346123c4a127400c02b87570db23de
Heljä Lehtinen <lehthelj@gmail.com>
2023-11-17 12:53:03 +0200

    Edited citation class

--- src/entities/citation.py
+++ src/entities/citation.py
@@ -1,4 +1,4 @@
-
+"""Luokka, joka luo uuden sitaatin"""
 
 class Citation():
     def __init__(self, type:str, author:str, title:str, year:int):
@@ -7,14 +7,6 @@ class Citation():
         self.title = title
         self.year = year
 
-        self.__input_valid()
-
-    def __input_valid(self):
-        if type(self.year) != int:
-            raise ValueError("Type must be integer")
-        if self.year < 0:
-            raise ValueError("Year can't be negative")
-
     def __str__(self):
         return f"{self.type}, {self.author}, {self.title}, {self.year}"
-    
+
