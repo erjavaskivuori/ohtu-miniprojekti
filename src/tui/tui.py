b26df98814663d620db52d8624c971052e02048f
Vilijumi <ltoivone@local>
2023-11-17 14:38:10 +0200

    Adding usage and help messages to TUI. Also fix everything inside the class.

--- src/tui/tui.py
+++ src/tui/tui.py
@@ -1,15 +1,5 @@
 from enum import Enum
 
-usage = """ *** TERVETULOA BIB ***
-
-Käyttö:
-
-Valikko toimii antamalla kirjaimen ja toimimalla ohjeiden mukaan..
-
-kirjoita tähn jotain järkevää
-
-Apua komennolla [h,H,?]
-"""
 
 class Commands(Enum):
     QUIT = 0,
@@ -17,32 +7,64 @@ class Commands(Enum):
     LIST = 2,
     HELP = 3
 
-commands = {
-    'Q'	: Commands.QUIT,
-    'q'	: Commands.QUIT,
-    'A'	: Commands.ADD,
-    'a'	: Commands.ADD,    
-    'L'	: Commands.LIST,
-    'l'	: Commands.LIST,    
-    'H'	: Commands.HELP,
-    'h'	: Commands.HELP,
-    '?'	: Commands.HELP
-}
 
 class Tui:
+    """ Tui - Class for menu and user inputs in text mode """
+
+    greetings = """
+*** TERVETULOA BIBSELLIIN ***
+
+Luo, lajittele, muokkaa viitteitä ja vedosta niistä BiBTeX tiedosto.
+
+"""
+
+    usage = """
+Ohjelma toimii syöttämällä valikossa toimintoa vastaava yksittäinen kirjain.
+Näin valittu toiminto sitten kysyy käyttäjältä tarvittavat tiedot.
+"""
+
+    commands = {
+        'Q'	: Commands.QUIT,
+        'q'	: Commands.QUIT,
+        'A'	: Commands.ADD,
+        'a'	: Commands.ADD,    
+        'L'	: Commands.LIST,
+        'l'	: Commands.LIST,
+        'P'	: Commands.LIST,
+        'p'	: Commands.LIST,
+        'H'	: Commands.HELP,
+        'h'	: Commands.HELP,
+        '?'	: Commands.HELP
+    }
+
+    descriptions = {
+        Commands.QUIT : "Lopeta ohjelma",
+        Commands.ADD : "Lisää viite",
+        Commands.LIST : "Listaa viitteet",
+        Commands.HELP : "Tulosta ohjeet" 
+    }
+    
     def __init__(self):
-        print(usage)
+        print(self.greetings)
+        self.help()
         
     def help(self):
-        print("helppi")
+        print(self.usage)
+        print("Komento:        Toiminto:")
+        for desc in self.descriptions.keys():
+            keys = ""
+            for comm in self.commands:
+                if self.commands[comm]==desc:
+                    keys+=f"[{comm}]"
+            print(f"{keys:16s}{self.descriptions[desc]}")
         
     def menu(self):
         while True:
-            key = input("Anna komento: ")
-            if key in commands.keys():
+            key = input("(VALIKKO) Anna komento: ")
+            if key in self.commands.keys():
                 break
-            print(f"Komento {key} ei kelpaa. Apua komennoilla [?,h,H]")
-        return commands[key]
+            print(f"Komento {key} ei kelpaa. Anna [?][h][H] saadaksesi apua.")
+        return self.commands[key]
         
     def ask(self, question :str, validator = lambda a: True):
         while True:
@@ -53,8 +75,5 @@ class Tui:
         return a
 
 
-
-            
-        
 if __name__ == "__main__":
     pass
