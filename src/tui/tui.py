2fd443a906807254ff83916f111bee0beda18486
Vilijumi <psaarin3@local>
2023-11-19 17:51:13 +0200

    Make TUI resemble everyone's favourite application fdisk

--- src/tui/tui.py
+++ src/tui/tui.py
@@ -1,38 +1,34 @@
 from enum import Enum
 
-
 class Commands(Enum):
     QUIT = 0,
     ADD = 1,
     LIST = 2,
     HELP = 3
 
-
 class Tui:
     """ Tui - Class for menu and user inputs in text mode """
 
     greetings = """
-*** TERVETULOA BIBSELLIIN ***
+[32m*** TERVETULOA BIBSELLIIN ***[39m
 
 Luo, lajittele, muokkaa viitteitä ja vedosta niistä BiBTeX tiedosto.
 
 """
 
     usage = """
-Ohjelma toimii syöttämällä valikossa toimintoa vastaava yksittäinen kirjain.
-Näin valittu toiminto sitten kysyy käyttäjältä tarvittavat tiedot.
+Ohjelman käyttöliittymä mukailee jokaisen suosikkiohjelma fdisk:iä.
+Ohjelmaa siis käytetään niin, että valikossa syötetään toimintoa vastaava
+yksittäinen kirjain. Valittu toiminto sitten kyselee toiminnon suorittamiseen
+tarvittavat tiedot.
 """
 
     commands = {
-        'Q': Commands.QUIT,
         'q': Commands.QUIT,
-        'A': Commands.ADD,
         'a': Commands.ADD,
-        'L': Commands.LIST,
-        'l': Commands.LIST,
-        'P': Commands.LIST,
         'p': Commands.LIST,
-        'H': Commands.HELP,
+        'l': Commands.LIST,
+        'm': Commands.HELP,
         'h': Commands.HELP,
         '?': Commands.HELP
     }
@@ -41,39 +37,45 @@ Näin valittu toiminto sitten kysyy käyttäjältä tarvittavat tiedot.
         Commands.QUIT: "Lopeta ohjelma",
         Commands.ADD: "Lisää viite",
         Commands.LIST: "Listaa viitteet",
-        Commands.HELP: "Tulosta ohjeet"
+        Commands.HELP: "Tulosta valikko/ohjeet"
+    }
+    
+    categories = {
+        "Viitteet": [Commands.ADD, Commands.LIST],
+        "Sekalaista": [Commands.HELP],
+        "Tallenna & Lopeta": [Commands.QUIT]
     }
 
     def __init__(self):
         print(self.greetings)
-        self.help()
 
     def help(self):
         print(self.usage)
-        print("Komento:        Toiminto:")
-        for desc in self.descriptions.keys():
-            keys = ""
-            for comm in self.commands:
-                if self.commands[comm] == desc:
-                    keys += f"[{comm}]"
-            print(f"{keys:16s}{self.descriptions[desc]}")
+        print("\nApu:")
+        for cat in self.categories:
+            print(f"\n  {cat}:")
+        
+            for desc in self.categories[cat]:
+                keys = []
+                for comm in self.commands:
+                    if self.commands[comm] == desc:
+                        keys.append(comm)
+                print(f"   {keys[0]}   {self.descriptions[desc]:40s}", end="")
+                print(f"[myös: {', '.join(keys[1:])}]" if len(keys)>1 else "")
 
     def menu(self):
         while True:
-            key = input("(VALIKKO) Anna komento: ")
+            key = input("\nKomento (apu: syötä m): ")
             if key in self.commands.keys():
                 break
-            print(f"Komento {key} ei kelpaa. Anna [?][h][H] saadaksesi apua.")
+            print("""[31m"""f"{key}: tuntematon komento.""""[39m""")
         return self.commands[key]
 
     def ask(self, question: str, validator=lambda a: True):
         while True:
-            a = input(f"Anna {question}: ")
+            a = input(f"\nSyötä {question}: ")
             if a != "" and validator(a):
                 break
-            print(f"Syöte ei kelpaa")
+            print("""[31m"""f"Syöte '{a}' ei kelpaa.""""[39m""")
         return a
 
-
-if __name__ == "__main__":
-    pass
