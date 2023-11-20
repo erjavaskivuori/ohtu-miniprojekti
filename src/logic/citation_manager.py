58e401b0db0d45694ca3100ce8a48e1e9ca8cc6a
TommiTH <tommithyyrylainen@gmail.com>
2023-11-20 16:00:47 +0200

    kommentoitiin ei implementoituja funktioita

--- src/logic/citation_manager.py
+++ src/logic/citation_manager.py
@@ -15,11 +15,11 @@ class CitationManager():
         for citation in self.all:
             print(citation)
 
-    def delete_citation(self, citation):
-        None
+#    def delete_citation(self, citation):
+#        None
 
-    def search_citation(self, citation):
-        None
+#    def search_citation(self, citation):
+#        None
 
 # if __name__=="__main__":
 #     testi = Citation("a","a","a",11)
