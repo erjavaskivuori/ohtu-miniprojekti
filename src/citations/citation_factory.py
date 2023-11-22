933b17807fba794adedcf47c49f7e7f963f299c6
TommiTH <tommithyyrylainen@gmail.com>
2023-11-22 18:33:11 +0200

    citations in their own folder, pylint green

---
+++ src/citations/citation_factory.py
@@ -0,0 +1,45 @@
+from citations.citation_type import CitationType
+from citations.new_citation import Citation
+
+# named in a way that .bib accepts
+# Could be moved to a file of it's own
+AUTHOR = "author"
+TITLE = "title"
+YEAR = "year"
+JOURNAL_TITLE = "journaltitle"
+BOOK_TITLE = "booktitle"
+
+# Requiredfields: author,title,year/date
+BOOK_STRINGS = [AUTHOR, TITLE, YEAR]
+# Requiredfields: author,title,journaltitle,year/date
+ARTICLE_STRINGS = [AUTHOR, TITLE, JOURNAL_TITLE, YEAR]
+# Requiredfields: author,title,booktitle,year/date
+INPROCEEDINGS_STRINGS = [AUTHOR, TITLE, BOOK_TITLE, YEAR]
+# --------------------------------------
+
+
+class CitationFactory():
+
+    def get_new_citation(self, citation_type: CitationType):
+        # hopefully temporary "if else hell":
+        match citation_type:
+            case CitationType.BOOK:
+                return Citation(citation_type, BOOK_STRINGS)
+            case CitationType.ARTICLE:
+                return Citation(citation_type, ARTICLE_STRINGS)
+            case CitationType.INPROCEEDINGS:
+                return Citation(citation_type, INPROCEEDINGS_STRINGS)
+        # default to book for now
+        return Citation(citation_type, BOOK_STRINGS)
+
+# Biblatex documentation:
+# http://mirrors.ctan.org/macros/latex/contrib/biblatex/doc/biblatex.pdf
+# (Entry types, starting from page 9)
+# Example, Book:
+# Requiredfields: author,title,year/date
+# Optional fields: editor,editora,editorb,editorc,translator,
+#   annotator,commentator,introduction,foreword,afterword, subtitle,
+#   titleaddon,maintitle,mainsubtitle,maintitleaddon, language,
+#   origlanguage,volume,part,edition,volumes,series, number,note,publisher,
+#   location,isbn,eid,chapter,pages, pagetotal,addendum,pubstate,doi,eprint,eprintclass,
+#   eprinttype,url,urldate
---
+++ src/citations/new_citation.py
@@ -0,0 +1,31 @@
+from citations.citation_type import CitationType
+
+class CitationAttribute:
+    def __init__(self, name: str):
+        self.name = name
+        self.value = ""
+
+    def get_value(self):
+        return self.value
+
+    def get_name(self):
+        return self.name
+
+    def set_value(self, value: str):
+        self.value = value
+
+    def __str__(self):
+        return self.name + ": " + self.value
+
+class Citation():
+    def __init__(self, citation_type: CitationType, attribute_names: list[str]):
+        self.type = citation_type
+        self.attributes: list[CitationAttribute] = []
+        for s in attribute_names:
+            self.attributes.append(CitationAttribute(s))
+
+    def __str__(self):
+        return_string = ""
+        for attribute in self.attributes:
+            return_string += str(attribute) + "\n"
+        return return_string
