667985e90256a366e72b0e64024717cb53456c81
TommiTH <tommithyyrylainen@gmail.com>
2023-11-29 18:59:56 +0200

    bibtex_maker.py ready for code review and integration

--- src/citations/bibtex_maker.py
+++ src/citations/bibtex_maker.py
@@ -1,28 +1,36 @@
-from new_citation import Citation, CitationType
-from citation_factory import CitationFactory
+from citations.new_citation import Citation
 
 WRITE_COMMAND = "w+"
 
 class BibTexMaker():
-    
+    """Luo .bib tiedostoja. Käyttö:
+    1: Importtaa BibTexMaker
+    2: Kutsu BibTexMake.generate_bible_text_file(sitaattilista, filun nimi)
+    3: ???
+    4: Tiedosto ilmestyy /src kansioon
+    """
+
     @staticmethod
-    def generate_bible_text_file(citationList: list[Citation], fileName: str):
-        textFile = open(fileName + ".bib",WRITE_COMMAND)
-        text = ""
-        for citation in citationList:
-            text += BibTexMaker.__generate_citation_text(citation)
-        print(text)
-        textFile.write(text)
+    def generate_bible_text_file(citation_list: list[Citation], file_name: str):
+        """
+        Luo .bib päätteisen tekstitiedoston listasta Citation olioita. 
+        Annetun filenamen ei tarvitse/kannata sisältää .bib päätettä.
+        """
+        with open(file_name + ".bib", WRITE_COMMAND, encoding="utf-8") as text_file:
+            text = ""
+            for citation in citation_list:
+                text += BibTexMaker.__generate_citation_text(citation)
+            print(text)
+            text_file.write(text)
 
     @staticmethod
     def __generate_citation_text(citation: Citation):
-        start = "@" + citation.type.name + "{" + citation.label + ",\n"
+        """Luo yhden sitaatin bibtex formaatissa, palauttaa str.
+        """
+        start = "@" + citation.type.name.lower() + "{" + citation.label + ",\n"
         middle = ""
         for attribute in citation.attributes:
-            middle += attribute.get_name() + " = {" + attribute.get_value() +"},\n"
+            middle += attribute.get_name() + \
+                " = {" + attribute.get_value() + "},\n"
         end = "}\n\n"
         return start + middle + end
-    
-if __name__ == "__main__":
-    citations = {CitationFactory.get_new_citation(CitationType.ARTICLE), CitationFactory.get_new_citation(CitationType.BOOK)}
-    BibTexMaker.generate_bible_text_file(citations, "bibtesti")
