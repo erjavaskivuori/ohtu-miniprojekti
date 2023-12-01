e9cd0a4740c5a56c89b969ebaf49c21305b71d3d
Paavo Kivioja <pkivioj2@local>
2023-12-01 17:46:11 +0200

    Making tui:yesno simpler.

--- src/tui/tui.py
+++ src/tui/tui.py
@@ -5,26 +5,27 @@ from enum import Enum
 
 class Commands(Enum):
     """ Commands """
-    QUIT =	0
-    ADD =	1
-    LIST =	2
-    HELP =	3
-    TAG =	4
-    BIB =	5
-    SEARCH =	6
+    QUIT = 0
+    ADD = 1
+    LIST = 2
+    HELP = 3
+    TAG = 4
+    BIB = 5
+    SEARCH = 6
+
 
 class ANSI:
-    reset=	"\033[0m"
-    bold=	"\033[1m"
-    red=	"\033[31m"
-    green=	"\033[32m"
-    magenta=	"\033[35m"
+    reset = "\033[0m"
+    bold = "\033[1m"
+    red = "\033[31m"
+    green = "\033[32m"
+    magenta = "\033[35m"
 
 
 class Tui():
     """ Tui - Class for menu and user inputs in text mode """
 
-    greetings = ANSI.green+"*** TERVETULOA BIBSELLIIN ***"+ANSI.reset+"""
+    greetings = ANSI.green + "*** TERVETULOA BIBSELLIIN ***" + ANSI.reset + """
 
 Luo, lajittele, muokkaa viitteitä ja vedosta niistä BiBTeX tiedosto.
 """
@@ -87,7 +88,7 @@ tarvittavat tiedot."""
                         keys.append(key)
                 self.output(f"   {keys[0]:8s}   {self.descriptions[desc]:40s}")
                 self.output(f"[myös: {', '.join(keys[1:])}]\n"
-                        if len(keys) > 1 else "\n")
+                            if len(keys) > 1 else "\n")
         self.output("\n")
 
     def menu(self):
@@ -99,10 +100,10 @@ tarvittavat tiedot."""
             except EOFError:		# Make Ctrl-D work exit
                 self.output("\n")
                 key = "\0"
-            except KeyboardInterrupt:	# Make Ctrl-C reset input
+            except KeyboardInterrupt:  # Make Ctrl-C reset input
                 self.output("\n")
                 continue
-            if key == "\0": # Fast escape used ony by tests
+            if key == "\0":  # Fast escape used ony by tests
                 return "\0"
             if key in self.commands:
                 break
@@ -111,16 +112,16 @@ tarvittavat tiedot."""
 
     def ask(self, question: str, validator=lambda a: True):
         """ ask(str, func) - ask user for input and validates it 
-        
+
             str:	String to output before asking for input
             func:	Validator function for the sting.
                         Defaults to the one which returns always true.
-        
+
         """
         while True:
             self.output(f"\nSyötä {question}: ")
             a = self.input()
-            if a == "\0": # Fast escape used ony by tests
+            if a == "\0":  # Fast escape used ony by tests
                 return "\0"
             if a != "" and validator(a):
                 break
@@ -129,39 +130,35 @@ tarvittavat tiedot."""
 
     def yesno(self, question):
         """ ask(str) - ask yes/no question 
-        
+
             str:	String to output before asking for input
-        
+
         """
         while True:
+            ok = ["kyllä", "yes", "k", "y", "joo", "ok", "sopii"]
+            no = ["ei", "no", "e", "n", "\0" ]
             self.output(f"\n{question}: ")
             a = self.input()
-            if a == "\0" or a.lower() == "ei" or a.lower() == "no" \
-                    or a.lower() == "e" or a.lower() == "n":
+            if a.lower() in no:
                 return False
-            if a.lower() == "kyllä" or a.lower() == "yes" \
-                    or a.lower() == "k" or a.lower() == "y":
+            if a.lower() in ok:
                 return True
-            self.print_error(f"Anna kyllä/ei")
+            self.print_error(f"Anna kyllä tai ei, '{a}' ei kelpaa.")
         return False
 
-
-    def print_item_entry(self, cite_id :str, txt :str):
+    def print_item_entry(self, cite_id: str, txt: str):
         """ print_item_entry() - For printing identifying line of citation"""
         self.output(f"\n{ANSI.magenta}id:{cite_id}\t{txt}{ANSI.reset}\n")
 
-
-    def print_item_attribute(self, key :str, value :str):
+    def print_item_attribute(self, key: str, value: str):
         """ print_item_attribute(key, value) -
                     for printing attributes of citation just after id line"""
         self.output(f"\t{key+':':30s}{value}\n")
 
-
-    def print(self, msg :str):
+    def print(self, msg: str):
         """ print(msg) - Just for printing plain text """
-        self.output(str(msg)+"\n")
+        self.output(str(msg) + "\n")
 
-
-    def print_error(self, msg :str):
+    def print_error(self, msg: str):
         """ print_error(msg) - For printing ERROR messages in RED color """
         self.output(f"{ANSI.red}*** VIRHE: {msg}{ANSI.reset}\n")
