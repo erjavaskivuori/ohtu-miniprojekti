255e3eee3f25383c375ac3b35305f004942e3672
Vlle Syrjäläinen <ville.syrjalainen@gmail.com>
2023-12-08 10:29:12 +0200

    function to populate db with test data

---
+++ src/populate_database.py
@@ -0,0 +1,29 @@
+import sqlite3
+from config import DATABASE_FILE_PATH, POPULATE_CITATIONS_PATH
+
+
+def populate_citations_from_file(connection, file_path):
+    """Populate the 'citations' table with test data from a text file.
+
+    Args:
+        connection: SQLite database Connection object.
+        file_path (str): Path to the text file containing citation data.
+    """
+    cursor = connection.cursor()
+
+    with open(file_path, 'r', encoding='utf8') as file:
+        for line in file:
+            values = line.strip().split(',')
+
+            cursor.execute("""
+                INSERT INTO citations (label, type, author, title, year, journal_title, book_title)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+            """, values)
+
+    connection.commit()
+
+
+if __name__ == "__main__":
+    conn = sqlite3.connect(DATABASE_FILE_PATH)
+    populate_citations_from_file(conn, POPULATE_CITATIONS_PATH)
+    conn.close()
